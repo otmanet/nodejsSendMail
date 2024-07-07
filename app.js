ce3e31fabed101926f75ffe687a00ec7a9b497e9
kaoutarfk <otmanetahri1@gmail.com>
2024-07-07 18:41:54 +0100

    remove line mogan dont used

--- app.js
+++ app.js
@@ -2,7 +2,6 @@ const express = require("express");
 const { errorHandler } = require("./middleware/errorHandler.js");
 const app = express();
 const indexRoutes = require("./Routes/index.routes.js");
-const morgan = require("morgan");
 app.use(morgan("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
